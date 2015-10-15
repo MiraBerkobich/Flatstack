8a780ebd323e8d2648c6ea21e4772d3433e0d570
MiraBerkobich <YOUR@EMAIL.comgit config --global user.name YOUR>
2015-10-15 16:06:15 +0300

    add code

--- code/task1.rb
+++ code/task1.rb
@@ -1,2 +1,28 @@
 class Task1
+  require 'csv'
+  a=CSV.read('text')
+  puts "Введите столбец сортировки (name/prof/sex/age)"
+  STDOUT.flush
+
+  b = gets.chomp
+  c = b.to_s
+  if c == c.to_s
+    name = 0
+    prof = 1
+    sex = 2
+    age = 3
+    if c == "name"
+      d=name
+    elsif c=="prof"
+      d = prof
+    elsif c=="sex"
+      d = sex
+    elsif c=="age"
+      d = age
+    end
+    a.each{|element|puts element[d]}
+  end
+# rescue
+# puts "введите слово"
+# end
 end
